0b87a2c634897be44433fe398094cccfed302610
Lawgiver17 <ccolon105@yahoo.com>
2019-01-12 14:18:55 -0700

    added module.export, tested some DB stuffs

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,20 +1,21 @@
 // === HTML ROUTES ===
-
-
-// GET route to serve up home page
-app.get("/", function(req, res) {
+module.exports = function(app) {
+  // GET route to serve up home page
+  app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/main.html"));
   });
 
 // GET route to serve up about us/contact page
-app.get("/about", function(req, res) {
+  app.get("/about", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/about.html"));
   });
 
 
 // GET route to serve up play page
-app.get("/game", function(req, res) {
+  app.get("/game", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/game.html"));
   });
+} 
+
 
 
